ce3913f97fa31c3dfcfe96a076940fb22943d17c
Alizee <alizeesola.america@gmail.com>
2026-09-16 21:04:36 -0700

    Detalles de comentarios

--- Tablero.cpp
+++ Tablero.cpp
@@ -6,12 +6,12 @@
 
 using namespace std;
 
-// Declaración global del array tablero (8*8 lugares)
+//Declaración global del array tablero (8*8 casillas)
 char tablero[8][8];
 char blanco = 'o';
 char negro = 'x';
-char libre = ' ';
-char limite = '-';
+char libre = '-';
+char limite = ' ';
 //Contadores de captura
 int Captura_blanca, Captura_negra;
 
@@ -20,14 +20,14 @@ void mostrarTablero();
 
 void iniciaTablero()
 {
-    //Reiniciar contadores en caso de jugar una segunda partida
+    //Reinicio de contadores
     Captura_blanca = 0;
     Captura_negra = 0;
 
     //Recorrido de filas
     for (int filas = 0; filas < 8; filas++)
     {
-        // Recorrido de columnas (elementos)
+        //Recorrido de columnas (elementos)
         for (int ele = 0; ele < 8; ele++)
         {
             //Casillas oscuras (donde si pueden moverse las fichas)
@@ -74,19 +74,19 @@ void mostrarTablero()
     }
     //Números para identificar celdas en X
     cout << "   0  1  2  3  4  5  6  7" << endl;
-    cout << "Puntos - Blanco(o): " << Captura_blanca << " | Negro(x): " << Captura_negra << endl;
+    cout << "Puntos de Blanco(o): " << Captura_blanca << " | Negro(x): " << Captura_negra << endl;
 }
 
 bool evaluarGanador()
 {
     if(Captura_blanca == 12)
     {
-        cout << "\n¡El jugador BLANCO ha ganado por capturas!" << endl;
+        cout << "\nEl jugador BLANCO ha ganado por capturas" << endl;
         return true;
     }
     else if(Captura_negra == 12)
     {
-        cout << "\n¡El jugador NEGRO ha ganado por capturas!" << endl;
+        cout << "\nEl jugador NEGRO ha ganado por capturas" << endl;
         return true;
     }
     return false; 
@@ -95,8 +95,10 @@ bool evaluarGanador()
 //verifica si hay un salto disponible
 bool tieneSaltoDisponible(int x, int y, char jugador)
 {
+    //dirección en Y según quien está jugando
     int dirY = (jugador == blanco) ? -1 : 1;
     char oponente = (jugador == blanco) ? negro : blanco;
+    //dirección en X
     int saltosX[2] = {-2, 2}; 
     
     for (int i = 0; i < 2; i++) 
@@ -145,7 +147,7 @@ void turno()
         }
         else 
         {
-            cout << "¡SALTO MULTIPLE! Debes continuar comiendo con tu ficha en (" << x << " " << y << ")" << endl;
+            cout << "SALTO MULTIPLE: Debes continuar comiendo con tu ficha en (" << x << " " << y << ")" << endl;
         }
 
         cout << "Ingrese posicion de donde la movera (X [espacio] Y):" << endl;
@@ -189,9 +191,14 @@ void turno()
                 tablero[y][x] = libre;                   
                 tablero[enemigoY][enemigoX] = libre; 
 
-                if (jugador == blanco) Captura_blanca++;
-                else Captura_negra++;
-                
+                if (jugador == blanco)
+                {
+                    Captura_blanca++;
+                }
+                else 
+                {
+                    Captura_negra++; 
+                }
                 mostrarTablero();
 
                 // Evaluar salto múltiple
